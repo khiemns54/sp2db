53c3a42ca628f1ce90a8df8b80a2aee8b6ec0839
KhiemNS <khiem-nguyen@kayac.com>
2018-04-12 11:11:39 +0900

    remove uncessary code

--- lib/sp2db/non_model_table.rb
+++ lib/sp2db/non_model_table.rb
@@ -1,23 +1,6 @@
 module Sp2db
   class NonModelTable < BaseTable
 
-    def initialize opts={}
-      super(opts)
-    end
-
-    def active_record?
-      false
-    end
-
-    # Table name
-    def name
-      @name ||= super
-    end
-
-    def name=n
-      @name = n&.to_sym
-    end
-
     def config
       @config ||= Sp2db.config.non_model_tables[self.name] || super
     end
